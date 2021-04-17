37fd624927aa47eee8d68f6c976ae6fc289f4d0b
Brian Kubisiak <brian@kubisiak.com>
2021-04-17 07:37:39 -0700

    initial commit

---
+++ config.lua
@@ -0,0 +1,27 @@
+snapshots = {
+    {name = "base",  modules = {"alpine"}},
+    {name = "setup", modules = {"network", "patch",}},
+}
+
+path = {
+    run = "/home/bkubisiak/src/eib/eib-run.sh",
+    mod = "/home/bkubisiak/src/eib/mod",
+}
+
+alpine = {
+    ARCH = "aarch64",
+    KEYS = "/home/bkubisiak/src/aports/main/alpine-keys",
+    MIRROR = "http://10.10.0.4/alpine",
+    VERSION = "v3.12",
+    PACKAGES = "alpine-base dropbear erlang wireguard-tools",
+    -- RMPACKAGES = "",
+    SERVICES = "dropbear",
+}
+
+network = {
+    DEVNAME = "foobar",
+}
+
+patch = {
+    DIRECTORIES = "firmware/",
+}
